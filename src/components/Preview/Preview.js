89213bd58918bff3194eba926ad78cf4e980d3d4
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2020-01-14 09:11:31 +0700

    bug fixed on preview component

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -13,24 +13,33 @@ export default class Preview extends Component {
             item: ''
         }
 
-        this.youtubeIframe = null;
+        this.previewItem = null;
+        this.hide = this.hide.bind(this);
     }
 
     show(item) {
         this.setState({
             item,
             show: true
+        }, () => {
+            document.addEventListener('click', this.hide);
         });
     }
 
-    hide() {
-        // stop playing video
-        const iframe = document.getElementById('youtubeiframe');
-        iframe.contentWindow.postMessage(JSON.stringify({ event: 'command', func: 'stopVideo' }), '*');
-
-        this.setState({
-            show: false
-        });
+    hide(e) {
+        if(!this.previewItem.contains(e.target)){
+            // stop playing video
+            const iframe = document.getElementById('youtubeiframe');
+            if(iframe !== null){
+                iframe.contentWindow.postMessage(JSON.stringify({ event: 'command', func: 'stopVideo' }), '*');
+            }
+        
+            this.setState({
+                show: false
+            }, () => {
+                document.removeEventListener('click', this.hide);
+            });
+        }
     }
 
     renderItem(item){
@@ -38,11 +47,12 @@ export default class Preview extends Component {
             return <h3>Content not available.</h3>;
         }
         else if(item.startsWith('https://www.youtube.com/')){
-            return <iframe id="youtubeiframe" width="100%" height="100%" src={item} allowfullscreen="allowfullscreen"
+            return <iframe id="youtubeiframe" width="100%" height="100%" src={item} 
+                        allowfullscreen="allowfullscreen" ref={(element) => {this.previewItem = element}}
                         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"/>
         }
         else {
-            return <img src={item} alt="Preview"/>
+            return <img src={item} alt="Preview" ref={(element) => {this.previewItem = element}}/>
         }
     }
 
@@ -51,7 +61,7 @@ export default class Preview extends Component {
         return(
             <div class="preview" style={display}>
                 <div class="chorizontal close">
-                    <img src={X} role="button" onClick={() => this.hide()} alt="Close"/>
+                    <img src={X} role="button" onClick={() => this.hide(this)} alt="Close"/>
                 </div>
                 <div class="item">
                     {this.renderItem(this.state.item)}
