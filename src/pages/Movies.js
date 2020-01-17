5c2ea138e49d1b51b014ff48212573971eb0a172
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2020-01-17 16:58:29 +0700

    working on lazy load

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import Menubar from '../components/Menubar';
 import Genres from '../components/Genres';
 import Gallery from '../components/Gallery';
+import Poster from '../components/Poster';
 import Footer from '../components/Footer';
 import MovieService from '../services/MovieService.js';
 import * as Lib from '../utils/Lib.js';
@@ -15,51 +16,67 @@ export default class Movies extends Component {
         super(props);
 
         this.state = {
+            discovering: false,
+            movies: [],
+
             showingMovies: [],
             indonesianMovies: [],   // add dynamic localization
             popularMovies: [],
             upcomingMovies: [],
         }
 
+        this.page = 1;
         this.movieService = new MovieService();
         this.discover = null;
         this.handleScroll = this.handleScroll.bind(this);
     }
 
     componentDidMount() {
-        this.defaultFeed();
         window.addEventListener('scroll', this.handleScroll);
+        this.handleParameters();
     }
 
     componentWillUnmount() {
         window.removeEventListener('scroll', this.handleScroll);
     }
 
-    handleParameters(){
+    handleParameters() {
         const display = Lib.getParameter(this, 'display');
-        const page = Lib.getParameter(this, 'page');
-
-        if(display !== null){
-
+        if(display === null){
+            this.defaultFeed();
+            return;
         }
+
+        this.setState({
+            discovering: true
+        });
+
+        if(display.toLowerCase() === 'showing'){
+            this.movieService.getShowing(this.page, (movies) => {
+                this.setState({
+                    movies
+                });
+            });
+        }
+
     }
 
     defaultFeed() {
         this.movieService.getShowing(1, (movies) => {
             this.setState({
-                showingMovies: Lib.more(movies, '/movie?display=showing,page=1')
+                showingMovies: Lib.more(movies, '/movie?display=showing')
             });
         });
 
         this.movieService.getIndonesianMovies(1, (movies) => {
             this.setState({
-                indonesianMovies: Lib.more(movies, '/movie?display=indonesia,page=1')
+                indonesianMovies: Lib.more(movies, '/movie?display=indonesia')
             });
         });
 
         this.movieService.getPopularMovies(1, (movies) => {
             this.setState({
-                popularMovies: Lib.more(movies, '/movie?display=popular,page=1')
+                popularMovies: Lib.more(movies, '/movie?display=popular')
             });
         });
 
@@ -78,8 +95,77 @@ export default class Movies extends Component {
     handleScroll() {
         const element = document.getElementById('content-movies');
         if (this.isBottom(element)) {
+            this.page += 1;
+            this.movieService.getShowing(this.page, (movies) => {
+                let moreMovies = this.state.movies;
+                moreMovies.push(...movies);
+                this.setState({
+                    movies: moreMovies
+                });
+            });
             console.log('header bottom reached');
             document.removeEventListener('scroll', this.handleScroll);
+        }
+        // TODO: NEED A LOT OF REFACTORING. Consider to add Loading component, and add 'safe fetch load' logic
+    }
+
+    renderContent(discovering){
+        if(discovering) {
+            return(
+                <Fragment>
+                    <div class="title-section">
+                        <h3>Results</h3>
+                    </div>
+                    <div class="movies">
+                        {
+                            this.state.movies.map((movie, i) => (
+                                <Poster key={movie.id} movie={movie}/>
+                            ))
+                        }
+                    </div>
+
+                    
+                </Fragment>
+            );
+        }
+        else {
+            return(
+                <Fragment>
+                    <div class="title-section">
+                        <h3>Genres</h3>
+                    </div>
+                    <Genres/>
+
+                    <div class="title-section">
+                        <h3>Now Playing</h3>
+                        <a href="/movie?display=showing">
+                            <span class="link">See All</span>
+                        </a>
+                    </div>
+                    <Gallery movies={this.state.showingMovies}/>
+
+                    <div class="title-section">
+                        <h3>Indonesia</h3>
+                        <a href="/movie?display=indonesia">
+                            <span class="link">See All</span>
+                        </a>
+                    </div>
+                    <Gallery movies={this.state.indonesianMovies}/>
+
+                    <div class="title-section">
+                        <h3>Most Popular</h3>
+                        <a href="/movie?display=popular">
+                            <span class="link">See All</span>
+                        </a>
+                    </div>
+                    <Gallery movies={this.state.popularMovies}/>
+
+                    <div class="title-section">
+                        <h3>Coming Soon</h3>
+                    </div>
+                    <Gallery movies={this.state.upcomingMovies}/>
+                </Fragment>
+            );
         }
     }
 
@@ -90,39 +176,7 @@ export default class Movies extends Component {
                 <Discover ref={(ref) =>{this.discover = ref}}/>
                 <div id="content-movies" class="content">
 
-                    <div class="title-section">
-                        <h3>Genres</h3>
-                    </div>
-                    <Genres/>
-
-                    <div class="title-section">
-                        <h3>Now Playing</h3>
-                        <a href="/movie?display=showing,page=1">
-                            <span class="link">See All</span>
-                        </a>
-                    </div>
-                    <Gallery movies={this.state.showingMovies}/>
-
-                    <div class="title-section">
-                        <h3>Indonesia</h3>
-                        <a href="/movie?display=indonesia,page=1">
-                            <span class="link">See All</span>
-                        </a>
-                    </div>
-                    <Gallery movies={this.state.indonesianMovies}/>
-
-                    <div class="title-section">
-                        <h3>Most Popular</h3>
-                        <a href="/movie?display=popular,page=1">
-                            <span class="link">See All</span>
-                        </a>
-                    </div>
-                    <Gallery movies={this.state.popularMovies}/>
-
-                    <div class="title-section">
-                        <h3>Coming Soon</h3>
-                    </div>
-                    <Gallery movies={this.state.upcomingMovies}/>
+                    {this.renderContent(this.state.discovering)}
 
                     <Footer/>
                 </div>
