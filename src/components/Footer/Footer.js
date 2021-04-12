902fc4c230cb7675c73f88f644aad8949e2ba6aa
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2021-04-12 10:50:22 +0700

    updated TMDB attribution

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Footer.css';
+import tmdb from '../../images/tmdb.svg'
 
 
 export default function Footer() {
@@ -7,7 +8,10 @@ export default function Footer() {
     return(
         <div class="footer">
             <p>© 2021 Happy Indra Wijaya. All rights reserved. <a href="/credits">Credits</a>. </p>
-            <p>Powered by <a class="tmdb" href="https://www.themoviedb.org/" target="_blank" rel="noopener noreferrer">TMDb</a></p>
+            <p>Powered by &nbsp;
+                <a class="tmdb" href="https://www.themoviedb.org/" target="_blank" rel="noopener noreferrer">
+                <img src={tmdb} alt="TMDB"/>
+                </a></p>
         </div>
     );
 }
